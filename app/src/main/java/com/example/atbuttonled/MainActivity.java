778d2fd517ed471451b0c0736a6bde22ff34636f
Morten Voigt Jagd <mort237d@edu.easj.dk>
2020-05-27 11:05:08 +0200

    onDestroy

--- app/src/main/java/com/example/atbuttonled/MainActivity.java
+++ app/src/main/java/com/example/atbuttonled/MainActivity.java
@@ -4,30 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.things.pio.Gpio;
 import com.google.android.things.pio.PeripheralManager;
 
 import java.io.IOException;
+import java.util.zip.ZipInputStream;
 
-/**
- * Skeleton of an Android Things activity.
- * <p>
- * Android Things peripheral APIs are accessible through the PeripheralManager
- * For example, the snippet below will open a GPIO pin and set it to HIGH:
- * <p>
- * PeripheralManager manager = PeripheralManager.getInstance();
- * try {
- * Gpio gpio = manager.openGpio("BCM6");
- *
- * gpio.setValue(true);
- * } catch (IOException e) {
- * Log.e(TAG, "Unable to access GPIO");
- * }
- * <p>
- * You can find additional examples on GitHub: https://github.com/androidthings
- */
 public class MainActivity extends Activity {
+    private static final String TAG = "MainActivity";
+    private Gpio redLED;
+    private Gpio greenLED;
+    private Gpio blueLED;
+    private Gpio redBTN;
+    private Gpio greenBTN;
+    private Gpio blueBTN;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,18 +29,18 @@ public class MainActivity extends Activity {
         PeripheralManager manager = PeripheralManager.getInstance();
 
         try {
-            Gpio redLED = manager.openGpio("BCM19");
+            redLED = manager.openGpio("BCM19");
             redLED.setDirection(Gpio.DIRECTION_OUT_INITIALLY_HIGH);
-            Gpio greenLED = manager.openGpio("BCM16");
+            greenLED = manager.openGpio("BCM16");
             greenLED.setDirection(Gpio.DIRECTION_OUT_INITIALLY_HIGH);
-            Gpio blueLED = manager.openGpio("BCM20");
+            blueLED = manager.openGpio("BCM20");
             blueLED.setDirection(Gpio.DIRECTION_OUT_INITIALLY_HIGH);
 
-            Gpio redBTN = manager.openGpio("BCM4");
+            redBTN = manager.openGpio("BCM4");
             redBTN.setDirection(Gpio.DIRECTION_IN);
-            Gpio greenBTN = manager.openGpio("BCM17");
+            greenBTN = manager.openGpio("BCM17");
             greenBTN.setDirection(Gpio.DIRECTION_IN);
-            Gpio blueBTN = manager.openGpio("BCM27");
+            blueBTN = manager.openGpio("BCM27");
             blueBTN.setDirection(Gpio.DIRECTION_IN);
 
             while (true){
@@ -58,6 +50,36 @@ public class MainActivity extends Activity {
             }
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        if (redLED != null) {
+            try { redLED.close(); }
+            catch (IOException e) { Log.e(TAG, "Error on PeripheralIO API", e); }
+        }
+        if (greenLED != null) {
+            try { greenLED.close(); }
+            catch (IOException e) { Log.e(TAG, "Error on PeripheralIO API", e); }
+        }
+        if (blueLED != null) {
+            try { blueLED.close(); }
+            catch (IOException e) { Log.e(TAG, "Error on PeripheralIO API", e); }
+        }
+        if (redBTN != null) {
+            try { redBTN.close(); }
+            catch (IOException e) { Log.e(TAG, "Error on PeripheralIO API", e); }
+        }
+        if (greenBTN != null) {
+            try { greenBTN.close(); }
+            catch (IOException e) { Log.e(TAG, "Error on PeripheralIO API", e); }
+        }
+        if (blueBTN != null) {
+            try { blueBTN.close(); }
+            catch (IOException e) { Log.e(TAG, "Error on PeripheralIO API", e); }
         }
     }
 }
